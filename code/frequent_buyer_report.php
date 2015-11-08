b17f533b907182177894ba233d9d1d91ee6a5485
Chris Freas <cfreas@packetbusters.net>
2015-11-08 09:47:36 -0500

    completed frequent buyer report. can someone double check my SQL query to make sure it is grabbing the right data?

--- code/frequent_buyer_report.php
+++ code/frequent_buyer_report.php
@@ -3,21 +3,46 @@
 	include 'include/validate.php';
 	include 'include/dbconnect.php';
 
+	$today_long = date("l, F j Y");
+
+	$frequent_buyer_query = "select tbl_Customer.nameFirst,tbl_Customer.nameLast, ProductSales_v.customerId,count(ProductSales_v.customerId) as purchasecount,ProductSales_v.saleDate from tbl_Customer inner join ProductSales_v on tbl_Customer.customerId=ProductSales_v.customerId group by ProductSales_v.customerId order by count(ProductSales_v.customerId) desc,ProductSales_v.saleDate desc";
+	$frequent_buyer_query_result = $dbconn->query("$frequent_buyer_query");
+
 ?>
 <html>
 <head>
 <title>Frequent Buyer Report</title>
     <link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="css/starter-template.css" rel="stylesheet">
     <link href="css/signin.css" rel="stylesheet">
 </head>
 <body>
-<br/>
-<br/>
+<div class="container">
+<div class="col-md-10 col-md-offset-1 material-animated-card">
 <center>
-<h2>Not Done Yet.</h2>
+<h3>Frequent Buyer Report For <?php print $today_long;?></h3>
+<?php
+    $num_rows = mysqli_num_rows($frequent_buyer_query_result);
+	if( $num_rows == 0 ) {
+        error_message("Uh-oh, no frequent buyers!");
+    } else {
+		echo '<table class="table">';
+        echo '<tr><th>Customer Name</th><th>Number of Purchases</th><th>Last Purchase Date</th></tr>';
+        while( $row = mysqli_fetch_array($frequent_buyer_query_result) ) {
+            $fname = $row['nameFirst'];
+            $lname = $row['nameLast'];
+            $purchasecount = $row['purchasecount'];
+            $lastpurchase = $row['saleDate'];
+			echo '<tr><td>' . $fname . ' ' . $lname . '</td><td>' . $purchasecount . '</td><td>' . $lastpurchase . '</td></tr>';
+        }
+		echo '</table>';
+	}
+?>
 </center>
+</div>
+</div>
 
 <!-- Bootstrap core JavaScript
     ================================================== -->
@@ -25,7 +50,7 @@
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
 	<script src="js/prettify/prettify.js"></script>
-	
+
 </body>
 
 </html>
